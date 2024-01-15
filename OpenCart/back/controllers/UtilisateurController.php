0100f2c6955cade985a53550d86e129410cc7695
Zeineb Haraketi <61319733+ZeinebHaraketi@users.noreply.github.com>
2024-01-15 22:28:09 +0100

    Update UtilisateurController.php
    Done!

--- OpenCart/back/controllers/UtilisateurController.php
+++ OpenCart/back/controllers/UtilisateurController.php
@@ -83,40 +83,7 @@ function recupererUtilisateur($id)
 
 // La Modification
 
-// function modifierUtilisateur($id, $utilisateur)
-// {
-//     $sql = "UPDATE utilisateur SET firstName = :firstName, lastName = :lastName, address = :address, country = :country, phoneNumber = :phoneNumber, picture = :picture WHERE id = :Id";
-//     $db = config::getConnexion();
-    
-//     try {
-//         $query = $db->prepare($sql);
-//         $query->execute([
-//             'Id' => $utilisateur->getID(),
-//             'firstName' => $utilisateur->getFirstName(),
-//             'lastName' => $utilisateur->getLastName(),
-//             'address' => $utilisateur->getAddress(),
-//             'country' => $utilisateur->getCountry(),
-//             'phoneNumber' => $utilisateur->getPhoneNumber(),
-//             'picture' => $utilisateur->getPicture()
-//         ]);
-//         $query->bindValue(':Id', $id);
-//         $query->bindValue(':firstName', $utilisateur->getFirstName());
-//         $query->bindValue(':lastName', $utilisateur->getLastName());
-//         $query->bindValue(':address', $utilisateur->getAddress());
-//         $query->bindValue(':country', $utilisateur->getCountry());
-//         $query->bindValue(':phoneNumber', $utilisateur->getPhoneNumber());
-//         $query->bindValue(':picture', $utilisateur->getPicture());
 
-
-
-        
-        
-
-//         return $query->rowCount() > 0; 
-//     } catch (Exception $e) {
-//         die('Erreur: ' . $e->getMessage());
-//     }
-// }
 
 function modifierUtilisateur($id, $utilisateur)
 {
@@ -125,7 +92,6 @@ function modifierUtilisateur($id, $utilisateur)
     try {
         $query = $db->prepare($sql);
 
-    // Liez les paramètres à la requête en utilisant la même casse que celle de la colonne dans la base de données
         $query->bindValue(':Id', $id, PDO::PARAM_INT);
         $query->bindValue(':firstName', $utilisateur->getFirstName(), PDO::PARAM_STR);
         $query->bindValue(':lastName', $utilisateur->getLastName(), PDO::PARAM_STR);
@@ -134,12 +100,10 @@ function modifierUtilisateur($id, $utilisateur)
         $query->bindValue(':phoneNumber', $utilisateur->getPhoneNumber(), PDO::PARAM_STR);
         $query->bindValue(':picture', $utilisateur->getPicture(), PDO::PARAM_STR);
 
-    // Exécutez la requête
         $query->execute();
 
         return $query->rowCount() > 0;
     } catch (Exception $e) {
-    // Il est préférable de ne pas afficher les détails de l'erreur directement, mais plutôt de les logger
     error_log('Erreur dans modifierUtilisateur: ' . $e->getMessage());
     return false;
     }
